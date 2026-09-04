969df42f9479617120336b9a93227ceb3f4d5777
pd7691033798-ai <pd7691033798@gmail.com>
2026-09-04 23:35:37 +0530

    Register additional sovereign endpoints

--- main.go
+++ main.go
@@ -750,6 +750,7 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprintln(w, scanReport)
 	})
+	RegisterRepoEngineRoutes()
 
 	// अतिरिक्त सॉवरन एंडपॉइंट्स (डिकॉय और मास्टर एजेंट्स)
 	registerSovereignEndpoints(fortress)
